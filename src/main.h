bc7ebf05bef9ec18224b9b83c07e8c663aee9114
AdderallDev <adderalldev@gmail.com>
2014-10-30 22:19:54 +0400

    Fix MaxMoney and Stake %

--- src/main.h
+++ src/main.h
@@ -36,9 +36,9 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 1000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
-static const int64_t MAX_MONEY = 17000000 * COIN;
-static const int64_t COIN_YEAR_REWARD = 3 * CENT; // 3% per year
-static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.03 * COIN;	// 3% annual interest
+static const int64_t MAX_MONEY = 30000000 * COIN;
+static const int64_t COIN_YEAR_REWARD = 8 * CENT; // 3% per year
+static const int64_t MAX_MINT_PROOF_OF_STAKE = 8 * CENT;	// 3% annual interest
 static const int MODIFIER_INTERVAL_SWITCH = 2000;
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
